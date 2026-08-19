9bffe22cc037e42d9fb1fe18d4ab5736839b24ce
Michael John <mikejgraphics80792761@gmail.com>
2026-08-19 01:34:18 +0100

    Add root redirect from / to /spa/

--- invite_api/urls.py
+++ invite_api/urls.py
@@ -1,8 +1,10 @@
 """Root URL configuration."""
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/spa/", permanent=False)),
     path("admin/", admin.site.urls),
     path("invites/", include("invites.urls")),
     path("spa/", include("spa.urls")),
